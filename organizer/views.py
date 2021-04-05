8580158082d867519437bebf7689c1dfddb028d2
Dom Aquino <dom.aquino@outlook.com>
2021-04-05 17:06:01 +0800

    Django Training - Save Progress

---
+++ organizer/views.py
@@ -0,0 +1,19 @@
+from django.http.response import HttpResponse
+from .models import Tag
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    html_output = "<html>\n"
+    html_output += "<head>\n"
+    html_output += "    <title>Don't do this!</title>\n"
+    html_output += "</head>\n"
+    html_output += "<body\n"
+    html_output += "    <ul>\n"
+    for tag in tag_list:
+        html_output += "        <li>"
+        html_output += tag.name.title()
+        html_output += "</li>\n"
+    html_output += "    </ul>\n"
+    html_output += "</body>\n"
+    html_output += "</html>\n"
+    return HttpResponse(html_output)
