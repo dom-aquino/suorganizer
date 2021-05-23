50fb09475b760c5ea959f5e8b822d42f187afeb3
Dom Aquino <dom.aquino@outlook.com>
2021-05-23 12:09:59 +0800

    Fix syntax issues

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def redirect_root(request):
-    return redirect('blog_post_list')
+    url_path = reverse('blog_post_list')
+    return HttpResponseRedirect(url_path)
